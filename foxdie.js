54477104a261f931062010bc4f7f3c4fb159479d
Ray Hogan <rayhogan@gmail.com>
2019-02-24 22:04:10 +0000

    The Fulton Surface-to-Air Recovery System. I'm familiar with the theory.

--- foxdie.js
+++ foxdie.js
@@ -15,22 +15,89 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload() {
-
+    this.load.image('background', 'img/background.png');
+    this.load.image('ground', 'img/ground.png');
+    this.load.image('ladder', 'img/ladder.png');
+    this.load.image('player', 'img/player.png');
 }
 
+var player;
+var ground;
+var ladder;
+var cursor;
 
 function create() {
 
-    var GAMEOVER = this.add.text(300, 300, 'COMING SOON', {
-        fontSize: '20px',
-        fill: '#000',
-        fill: "#ffffff",
-    });
+    var self = this;
+
+    // Draw Scene
+    this.add.image(400, 300, 'background');
+    ladder = self.physics.add.sprite(380, 175, 'ladder');
+    ground = this.physics.add.staticGroup();
+    ground.create(400, 575, 'ground');
+
+    // Draw Player
+    player = self.physics.add.sprite(200, 200, 'player');
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+    player.body.setGravityY(600);
+
+    // Add collider
+    self.physics.add.collider(player, ground);
+
+    // Cursor keys
+    cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
 
+    if (player) {
+
+        if (cursors.left.isDown) {
+            player.setVelocityX(-160);
+
+        } else if (cursors.right.isDown) {
+            player.setVelocityX(160);
+
+        } else {
+            player.setVelocityX(0);
+        }
+
+        if (checkOverlap(player, ladder).width > 0) {
+
+            player.setGravityY(0);
+            if (cursors.up.isDown) {
+                player.setVelocityY(-200);
+            }
+            else if (cursors.down.isDown) {
+                player.setVelocityY(+200);
+            }
+            else {
+                player.setVelocityY(0);
+            }
+        }
+        else {
+            player.body.setGravityY(600);
+        }
+
+    }
 
 }
+
+function checkOverlap(spriteA, spriteB) {
+
+    try {
+        var boundsA = spriteA.getBounds();
+        var boundsB = spriteB.getBounds();
+
+        return Phaser.Geom.Rectangle.Intersection(boundsA, boundsB);
+    }
+    catch (e) {
+        console.log(e);
+        return false;
+    }
+
+}
+
 
 
